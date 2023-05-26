6a794fe01e47165590010f9123c07f8f7861b74c
Ignacio Ibanez <ibanez.ignaciom@gmail.com>
2023-05-26 20:58:38 -0300

    Modif home. Makemigrations

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpRequest, HttpResponse
+
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "home/index.html")
